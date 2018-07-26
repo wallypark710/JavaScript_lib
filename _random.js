d55332414d8ed2cb7a38d69aff513ad2bd2f99ab
walli <wallypark710@gmail.com>
2018-07-26 19:53:04 +0900

    write random function

---
+++ _random.js
@@ -0,0 +1,12 @@
+var getRandomInt = function( from, end ) {
+  return parseInt( Math.random() * ( end - from ) + from );
+}
+
+var randomString = function(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
